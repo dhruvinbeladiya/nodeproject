3af1a5765af756bb6df10db01784d6d10680a207
dhruvinbeladiya <dhruvin@gmail.com>
2023-10-18 19:11:58 +0530

    nodemailer

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,31 @@ const userModel = require("../model/userModel")
 var nodemailer = require('nodemailer');
 
 exports.insert = async(req,res) => {
+    var otp = Math.floor(1000 + Math.random() * 9000)
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'dhruvinbeladiya01@gmail.com',
+          pass: 'iooiqaicorkiamqu'
+        }
+      });
+      
+      var mailOptions = {
+        from: 'dhruvinbeladiya01@gmail.com',
+        to: req.body.email,
+        subject: 'Sending Email using Node.js',
+        text: otp.toString() 
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+
+
     var data = await userModel.create(req.body)
     res.status(200).json({
         data
